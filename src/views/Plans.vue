c2c1e02fffbf81c1412caf52246c1cf0adc6e42b
Alan-chenhx <chen1479391832@gmail.com>
2019-12-02 20:49:41 -0600

    search

--- src/views/Plans.vue
+++ src/views/Plans.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-content>
       <v-container >
-        <core-dialog/>
+        <core-search/>
         <v-data-iterator
           :items="items"
           :items-per-page.sync="itemsPerPage"
