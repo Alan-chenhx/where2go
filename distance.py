dd3f345fc21431648f964ff369b33db908ff10c2
Shuhan Wang <shuhanw2@illinois.edu>
2019-12-02 20:55:15 -0600

    update distance calculation

--- distance.py
+++ distance.py
@@ -1,4 +1,6 @@
 from math import sin, cos, sqrt, atan2, radians
+from tqdm import tqdm
+import json
 
 def calc_dist(p1, p2):
     lat1 = radians(p1[0])
@@ -12,4 +14,25 @@ def calc_dist(p1, p2):
     R = 6373.0
     return R * c
 
-print(calc_dist(p1, p2))
+with open('geo.json') as f:
+    json_dat = json.load(f)
+
+distance_dict = {}
+
+for city in json_dat:
+    distance_dict[city] = {}
+    places = json_dat[city]
+    for place in tqdm(places):
+        distance_dict[city][place] = {}
+        for otherplace in places:
+            if place == otherplace:
+                continue
+            else:
+                if 'lat' not in json_dat[city][place].keys() or 'lat' not in json_dat[city][otherplace].keys():
+                    continue
+                p1 = (json_dat[city][place]['lat'], json_dat[city][place]['lng'])
+                p2 = (json_dat[city][otherplace]['lat'], json_dat[city][otherplace]['lng'])
+                distance_dict[city][place][otherplace] = calc_dist(p1, p2)
+
+distance_json = json.dumps(distance_dict)
+print(distance_json)
