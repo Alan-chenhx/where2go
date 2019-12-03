f579b589b899dae44f95b75c857c6acdec674a30
Mingjie Zhao <mingjie7@illinois.edu>
2019-12-02 22:12:36 -0600

    update

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,7 @@ const routes = [{
     // route level code-splitting
     // this generates a separate chunk (login.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+    component: () => import( /* webpackChunkName: "login" */ '../views/Login.vue')
   },
   {
     path: '/plans',
@@ -36,7 +36,7 @@ const routes = [{
     // route level code-splitting
     // this generates a separate chunk (login.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "login" */ '../views/Plans.vue')
+    component: () => import( /* webpackChunkName: "login" */ '../views/Plans.vue')
   }
 ]
 
@@ -46,17 +46,17 @@ const router = new VueRouter({
   routes
 })
 
-  // router.beforeEach((to, from, next) => {
-  //   const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
-  //   const session = localStorage.getItem('currUserId');
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+  const session = localStorage.getItem('currUserId');
 
-  //   if (requiresAuth && !session) {
-  //     next('/login')
-  //   } else if (requiresAuth && session) {
-  //     next()
-  //   } else {
-  //     next()
-  //   }
-  // })
+  if (requiresAuth && !session) {
+    next('/login')
+  } else if (requiresAuth && session) {
+    next()
+  } else {
+    next()
+  }
+})
 
 export default router
