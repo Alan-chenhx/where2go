b76fbe78479c956e0b4d19807cbd5d1ff4dc490e
Shuhan Wang <shuhanw2@illinois.edu>
2019-12-02 21:20:21 -0600

    add weight to neo4j edges

--- add_weight.py
+++ add_weight.py
@@ -10,7 +10,7 @@ for city in json_dat:
     for place in json_dat[city]:
         for otherplace in json_dat[city][place]:
             query = """
-                MATCH (p1), (p2)
+                MATCH (p1:Place), (p2:Place)
                 WHERE p1.name = {place}, p2.name = {otherplace}
                 CREATE (p1)-[:WEIGHT {{weight: {weight}}}]->(p2)
             """
