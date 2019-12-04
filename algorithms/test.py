e7d5b0507f70c819c021391f6f5a3bc438ec419f
Mingjie Zhao <mingjie7@illinois.edu>
2019-12-03 19:07:42 -0600

    clean master

--- algorithms/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np;
-import json
-import pprint
-from random import randint
-with open('distance.json') as json_file:
-    dis = json.load(json_file)
-    dist = dis['Los Angeles']
-print(dist['Studio Tour']['Guinness World Records Museum'])
-
