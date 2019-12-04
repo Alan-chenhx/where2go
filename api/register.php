52997026e061e5c20d95c9481cdd28535f9587de
Alan-chenhx <chen1479391832@gmail.com>
2019-12-04 17:03:02 -0600

    php_register

--- api/register.php
+++ api/register.php
@@ -19,3 +19,21 @@ $query = mysqli_query($conn, "INSERT INTO profiles (email, description, avatar, 
                                                       name = '$name', 
                                                       avatar = '$avatar';");
 $query = mysqli_query($conn, "INSERT INTO users (username, password) VALUES ('$username', '$password');");
+
+
+$query = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username' AND password = '$password'");
+$counter = mysqli_num_rows($query);
+$response = array();
+if ($counter == 0) {
+    header('HTTP/1.1 401 Internal Server Booboo');
+    header('Content-Type: application/json; charset=UTF-8');
+    die(json_encode(array('message' => 'ERROR', 'code' => 1337)));
+} else {
+    $row = mysqli_fetch_array($query);
+    $user_id = $row['user_id'];
+    $_SESSION["user_id"] = $user_id;
+    $_SESSION["username"] = $username;
+    $response_array['success'] = 1;
+    $response_array['currUserId'] = $user_id;
+    echo json_encode($response_array);
+}
