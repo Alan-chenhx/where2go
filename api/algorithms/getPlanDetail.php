805d5dbfd3ab15b4ad9e90338f0055423bc0efb9
Shuhan Wang <wshuh126@gmail.com>
2019-12-05 03:45:39 -0600

    finish plan detail

--- api/algorithms/getPlanDetail.php
+++ api/algorithms/getPlanDetail.php
@@ -3,7 +3,7 @@
 $_POST = json_decode(file_get_contents("php://input"), true);
 $planId = $_POST['planId'];
 
-$to_exec = "python returnPlan.py 13458";
+$to_exec = "python returnPlan.py ".$planId;
 $command = escapeshellcmd($to_exec);
 $output = shell_exec($command);
 $file = file_get_contents("test.json");
