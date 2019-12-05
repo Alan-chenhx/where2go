320a6f0c0e772330141c850aac95a59d3e390f99
Shuhan Wang <wshuh126@gmail.com>
2019-12-05 06:54:10 -0600

    test

--- api/algorithms/getPlanDetail.php
+++ api/algorithms/getPlanDetail.php
@@ -3,7 +3,7 @@
 $_POST = json_decode(file_get_contents("php://input"), true);
 $planId = $_POST['planId'];
 
-$to_exec = "python returnPlan.py ".$planId;
+$to_exec = "python returnPlan.py 22212";
 $command = escapeshellcmd($to_exec);
 $output = shell_exec($command);
 $file = file_get_contents("test.json");
