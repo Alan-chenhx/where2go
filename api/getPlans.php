dc98e7e1e2788365a13b0fde19dd20d0ac1a6f79
Alan-chenhx <chen1479391832@gmail.com>
2019-12-05 00:13:37 -0600

    zmj laobi

--- api/getPlans.php
+++ api/getPlans.php
@@ -2,12 +2,13 @@
 
 include 'includes/db.php';
 // $_POST = json_decode(file_get_contents("php://input"),true);
-$uid = $_SESSION['user_id'];
-// $uid = 0;
+// $uid = $_SESSION['user_id'];
+$uid = 0;
 
 $query = mysqli_query($conn, "SELECT * FROM plans WHERE user_id = '$uid'");
 
 $counter = mysqli_num_rows($query);
+$response = array($counter);
 for ($x = 0; $x < $counter; $x++) {
     $row = mysqli_fetch_array($query);
     $response_array["start"] = $row["start"];
@@ -18,7 +19,10 @@ for ($x = 0; $x < $counter; $x++) {
     $response_array["pace"] = $row["pace"];
     $response_array["plan_id"] = $row["ref_id"];
     $response_array["cover"] = $row["cover"];
+    $response_array["name"] = "Jojo's Bizarre Adventure";
+    $response[$x]=$response_array;
 }
+// $ans["plans"]=$response;
 
 
-echo json_encode($response_array);
+echo json_encode($ans);
