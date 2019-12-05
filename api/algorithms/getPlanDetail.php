e21a704cb4e6c4044cb9bed53a7265531fc415a4
Alan-chenhx <chen1479391832@gmail.com>
2019-12-05 11:32:08 -0600

    Alan-chenhx

--- api/algorithms/getPlanDetail.php
+++ api/algorithms/getPlanDetail.php
@@ -3,7 +3,8 @@
 $_POST = json_decode(file_get_contents("php://input"), true);
 $planId = $_POST['planId'];
 
-$to_exec = "python3 returnPlan.py ".$planId;
+// $to_exec = "python3 returnPlan.py ".$planId;
+$to_exec = "python3 returnPlan.py 22471";
 $command = escapeshellcmd($to_exec);
 $output = shell_exec($command);
 // $file = file_get_contents("test.json");
