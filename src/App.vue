b4d1d52141b6d95d6ee4edd7cf21c2a66ee0ef95
Mingjie Zhao <mingjie7@illinois.edu>
2019-12-05 05:26:03 -0600

    fix

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,13 @@
 
 <style lang="scss">
 @import "@/styles/index.scss";
+.container {
+    -ms-overflow-style: none;  /* Internet Explorer 10+ */
+    scrollbar-width: none;  /* Firefox */
+}
+.container::-webkit-scrollbar { 
+    display: none;  /* Safari and Chrome */
+}
 </style>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-app>
-    <v-content>
+  <v-app >
+    <v-content >
       <v-carousel :show-arrows="false" hide-delimiters cycle height="100vh">
         <v-carousel-item v-for="(item,i) in backgrounds" :key="i" :src="item.src"></v-carousel-item>
       </v-carousel>
     </v-content>
     <v-content style="z-index:1;position:fixed;width:100%;height:100%">
-      <v-container class="fill-height" fluid>
+      <v-container class="fill-height" style="overflow-y: auto;" fluid>
         <v-row align="center" justify="center">
           <v-col cols="12" sm="8" md="5">
             <core-NewTripCard />
