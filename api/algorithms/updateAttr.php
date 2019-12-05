2a32b7c22c64b60614505cb9b2787914eac7dfb3
Shuhan Wang <wshuh126@gmail.com>
2019-12-05 03:46:06 -0600

    update attraction information

---
+++ api/algorithms/updateAttr.php
@@ -0,0 +1,21 @@
+<?php
+
+$_POST = json_decode(file_get_contents("php://input"), true);
+
+$city = $_POST['city'];
+$duration = $_POST['duration'];
+$oldname = $_POST['oldname'];
+$name = $_POST['name'];
+$planId = $_POST['planId'];
+
+$city = "Mountain View";
+$oldname = "17-Mile Drive";
+$name = "Pacific Grove Certified Farmers' Market";
+$duration = 233;
+$planId = 13458;
+
+$command = "python updateIter.py "."\"$city\"".' '."\"$oldname\"".' '."\"$name\"".' '.$duration.' '.$planId;
+$to_exec = escapeshellcmd($command);
+$output = shell_exec($to_exec);
+
+echo $output;
---
+++ api/algorithms/updateIter.py
@@ -0,0 +1,45 @@
+import sys
+import json
+from py2neo import Graph
+import math
+
+graph = Graph(host='localhost', auth=('neo4j', 'abduabdu'))
+
+city = sys.argv[1]
+oldname = sys.argv[2]
+name = sys.argv[3].replace('\\', '')
+duration = sys.argv[4]
+planId = sys.argv[5]
+
+query = """
+    MATCH (p:Place)-[:Near]->(c:City)
+    WHERE p.name = "%s" AND c.name = "%s"
+    RETURN ID(p)
+""" %(name, city)
+ref_id = graph.run(query).data()[0]['ID(p)']
+query = """
+    MATCH (i:Itinerary)
+    WHERE i.name = "%s" AND i.city = "%s" AND i.plan_id = %s
+    RETURN i.timetonext
+""" %(oldname, city, planId)
+timetonext = graph.run(query).data()[0]['i.timetonext']
+nextiter = ''
+newtime = 0
+if timetonext != 0:
+    query = """
+        MATCH (i:Itinerary)-[:Next]->(j:Itinerary)
+        WHERE i.name = "%s" AND i.city = "%s" AND i.plan_id = %s
+        RETURN j.name
+    """ %(oldname, city, planId)
+    nextiter = graph.run(query).data()[0]['j.name']
+    with open('distance.json', 'r') as f:
+        dat = json.load(f)
+    newtime = math.ceil(float(dat[city][name][nextiter]) * 1.6)
+
+query = """
+    MATCH (i:Itinerary)
+    WHERE i.name = "%s" AND i.city = "%s" AND i.plan_id = %s
+    SET i.name = "%s", i.duration = %s, i.ref_id = %s, i.timetonext = %s
+    RETURN i
+""" %(oldname, city, planId, name, duration, ref_id, newtime)
+graph.run(query)
